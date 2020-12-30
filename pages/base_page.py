1183bb8929732feecec388196caceb79f2ddccd6
ScrappyCocoJamba <konstantinmart10@gmail.com>
2020-12-30 16:32:22 +0700

    add some comments to file

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,15 +3,16 @@ from selenium.common.exceptions import NoSuchElementException
 
 class BasePage():
 
-    def __init__(self, browser, url, timeout=10):
+    def __init__(self, browser, url, timeout=10):  #конструктор — метод, который вызывается, когда мы создаем объект
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
 
-    def open(self):
+    def open(self):                             #открывает нужную страницу в браузере, используя метод get()
         self.browser.get(self.url)
 
     def is_element_present(self, how, what):
+        # Чтобы перехватывать исключение, нужна конструкция try/except
         try:
             self.browser.find_element(how, what)
         except (NoSuchElementException):
