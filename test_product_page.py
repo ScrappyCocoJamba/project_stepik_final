8bcf59cf136eff80ee75dd9cda950906d14a5e55
ScrappyCocoJamba <konstantinmart10@gmail.com>
2020-12-31 14:50:54 +0700

    some changes

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 from .pages.product_page import ProductPage
 import pytest
+
 """
 Можно реализовать с помощью цикла "for", зная базовый линк, с помощью f-строки добавляем
 диапазон id промо акции от 0 до 9
@@ -12,6 +13,7 @@ urls = [f"{product_base_link}/?promo=offer{no}" for no in range(10)]
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
 
+
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -19,7 +21,9 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?prom
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                   pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  pytest.param(
+                                      "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                      marks=pytest.mark.xfail),
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 def test_guest_can_add_product_to_basket(browser, link):
@@ -28,4 +32,3 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.add_to_basket()
     page.check_product_added()
     page.check_basket_price()
-
