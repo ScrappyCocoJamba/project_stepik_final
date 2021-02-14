35e83d6c4994c40bde709f3cf379283a42e81cb8
ScrappyCocoJamba <konstantinmart10@gmail.com>
2021-02-14 15:51:27 +0700

    concatenate to class with pytest.mark

--- test_main_page.py
+++ test_main_page.py
@@ -1,18 +1,29 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
+from .pages.product_page import ProductPage
+import  pytest
 
 # pytest -v --tb=line --language=en test_main_page.py::test_guest_cant_see_product_in_basket_opened_from_main_page
+# pytest -v --tb=line --language=en -m login_guest test_main_page.py
 
 link = "http://selenium1py.pythonanywhere.com/"
 
 
-def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, link)          # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                             # открываем страницу
-    page.go_to_login_page()         # выполняем метод страницы — переходим на страницу логина
-    login = LoginPage(browser, browser.current_url)  # Инициализируем LoginPage в теле теста
-    login.should_be_login_page()            # проверка ссылки на авторизацию
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+
+    def test_guest_can_go_to_login_page(self, browser):
+        page = MainPage(browser, link)          # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()                             # открываем страницу
+        page.go_to_login_page()         # выполняем метод страницы — переходим на страницу логина
+        login = LoginPage(browser, browser.current_url)  # Инициализируем LoginPage в теле теста
+        login.should_be_login_page()            # проверка ссылки на авторизацию
+
+    def test_guest_should_see_login_link_on_product_page(self, browser):
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
